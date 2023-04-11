0185e86b0097ef0c8ff7ff2b794760cc2fdc5af5
Felipe Rolon <afra.9622@gmail.com>
2023-04-11 14:55:19 -0400

    Python practice

---
+++ pythonPractice/practice.py
@@ -0,0 +1,20 @@
+
+# numberToSave = ''
+# while (type(numberToSave) != int) :
+#   numberToSave = (input("Input an integer: "))
+#   if ()
+
+# int(numberToSave)
+# print(type(numberToSave))
+
+# string = input("Input your string to detect: ")
+# if "  " in string:
+#     print("Double spaces detected!")
+# else:
+#     print("No double spaces found")
+
+# Create a tuple containing the fruit names
+fruits = (input("Enter fruit 1: "), input("Enter fruit 2: "), input("Enter fruit 3: "), input("Enter fruit 4: "), input("Enter fruit 5: "), input("Enter fruit 6: "), input("Enter fruit 7: "))
+
+# Print the tuple of fruits
+print("The tuple of fruits is:", fruits)
